6a542b30bf5624afc0f901bfe472aed92e099286
yancheng <410982205@qq.com>
2016-12-19 16:31:01 +0800

    引入ci框架

---
+++ application/controllers/Blog.php
@@ -0,0 +1,16 @@
+<?php
+class Blog extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+        $this->load->model('Blog_model');
+    }
+
+    public function index()
+    {
+    	//加载默认首页
+        echo '首页';
+    }
+}
---
+++ application/controllers/Login.php
@@ -0,0 +1,15 @@
+<?php
+class Login extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+    }
+
+    public function index()
+    {
+    	//登录
+        echo '登录页';
+    }
+}
